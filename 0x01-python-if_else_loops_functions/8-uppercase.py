d98859e299df8f0837336975013549ab421722ff
Sophie-analytics <sonibaresofiat@gmail.com>
2023-11-29 17:02:53 -0800

    convert uppercase to lowercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for alphabet in str:
-        if ord(alphabet) in range(97, 122):
+        if ord(alphabet) in range(97, 123):
             alphabet = chr(ord(alphabet) - 32)
         print("{:s}".format(alphabet), end='')
     print()
