1b94fc1262a45fca681d55c95fe6020776642a8c
Sophie-analytics <sonibaresofiat@gmail.com>
2023-12-07 03:54:52 +0100

    fix:indentation

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def only_diff_elements(set_1, set_2):
     diff_items = []
     for item1 in set_1:
         if item1 not in set_2:
-                diff_items.append(item1)
+            diff_items.append(item1)
     for item2 in set_2:
         if item2 not in set_1:
-                diff_items.append(item2)
+            diff_items.append(item2)
     return diff_items
