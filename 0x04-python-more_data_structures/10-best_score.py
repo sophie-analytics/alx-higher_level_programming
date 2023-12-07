629095bf6a412cf04555e1cfdce91dda63de2eb6
Sophie-analytics <sonibaresofiat@gmail.com>
2023-12-07 03:24:56 +0100

    looks for the key with the highest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    high_score = 0
+    high_key = None
+
+    for key, score in a_dictionary.items():
+        if score > high_score:
+            high_score = score
+            high_key = key
+    return high_key
