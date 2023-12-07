8670d90f84cbf89746cb38a2c232997a66806a38
Sophie-analytics <sonibaresofiat@gmail.com>
2023-12-07 01:59:58 +0100

    look for different elements in 2 sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,7 @@ def only_diff_elements(set_1, set_2):
     for item1 in set_1:
         if item1 not in set_2:
                 diff_items.append(item1)
-    for item2 in set_1:
+    for item2 in set_2:
         if item2 not in set_1:
                 diff_items.append(item2)
     return diff_items
