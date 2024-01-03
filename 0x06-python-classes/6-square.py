f700bcce404e97a1cede620989e541e643a06cd8
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-03 17:27:38 +0100

    created a new attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,28 +28,29 @@ class Square:
     @property
     def position(self):
         """Get/set the current position of the square."""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        if len(value) != 2:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        for index in range(1):
-            if value[index] < 0:
-                raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
         """Return the current area of the square."""
-        return self.__size * self.__size
-    
+        return (self.__size * self.__size)
+
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print("_", end="") for j in range(0, self.__position[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
