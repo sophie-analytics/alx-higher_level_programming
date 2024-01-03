e09ec60c6f60c4c7e62076d3545f9410e97d1991
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-03 01:40:46 +0100

    added a read me

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-    This is an empty class that defines a square
-"""
+""" This is an empty class that defines a square """
+
+
 class Square:
     """ This is the empty class """
     pass
