05a95b1a08285967d8bd2e760c39599ad74010da
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-03 02:05:02 +0100

    a classs that creates a private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,10 @@ a private attribute """
 
 class Square:
     """ object initialization """
-
-
+    
     def __init__(self, size):
         """Constructor.
         Args:
         size: length of side of the square.
         """
         self.__size = size
-  
