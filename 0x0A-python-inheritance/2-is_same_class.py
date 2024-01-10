7bcc2516206841d25b8a54cddd495cf5f164a42e
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-10 20:17:45 +0100

    compares class type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Checks for the nstance of a class"""
+
+
 def is_same_class(obj, a_class):
+   """ function for comparison"""
    if type(obj) == a_class:
       return True
    return False
