789bcc5787556d3d86daa7b559e8450d0c0884c6
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-10 21:27:23 +0100

    used raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defines a class basegeometry """
+
+
+class BaseGeometry:
+    """ representation of the class """
+    def area(self):
+        """  defines the area of a geometry"""
+        raise Exception("area() is not implemented")
+
