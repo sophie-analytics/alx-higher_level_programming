e3591cd90410a15451d5af71fbfad2406984d355
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-15 15:07:52 +0100

    fix: remove trailing whitespace

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,8 +74,8 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle") 
-        elif not isinstance(rect_2, Rectangle): 
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         area_1 = rect_1.area()
         area_2 = rect_2.area()
