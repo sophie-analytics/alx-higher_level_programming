cc8643c00a76d393685114fa986c23fbd7b83d87
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-17 13:49:59 +0100

    added a docstring

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Function that reads the content of the file """
 def read_file(filename=""):
+    """ args:
+    filename: The name of the file to be read"""
     with open(filename, encoding="utf-8") as f:
         data = f.read()
     print(data)
