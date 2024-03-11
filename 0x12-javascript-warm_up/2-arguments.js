d304412a57b2d17138d653183f4491094762d81a
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-11 21:18:22 +0100

    fixed an error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length == 1)
+if (process.argv.length == 2)
 {
     console.log('No argument');
 }
-else if (process.argv.length > 1)
+else if (process.argv.length > 2)
 {
     console.log('Arguments found');
 }
