9738e7702bd33e05917ee42b3b86ad7d550a4257
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-11 21:38:49 +0100

    fixed an error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[3]) {
-console.log(process.argv[3]);
-} else {
-console.log('No argument');
+if (process.argv[3])
+{
+    console.log(process.argv[3]);
+}
+else
+{
+    console.log('No argument');
 }
