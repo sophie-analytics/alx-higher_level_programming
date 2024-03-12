64648b0ef0f8ab19c8be69318262f808dd28261f
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-12 06:33:17 +0100

    fix: an error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ if (isNaN(parseInt(process.argv[2])) || process.argv[2] === undefined)
 }
 else
 {
-    console.log('My number: ', parseInt(process.argv[2]));
+    console.log('My number:', parseInt(process.argv[2]));
 }
