ea05d53e948ad07e6a52a3f96f41d32cbeb789a5
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-12 06:59:29 +0100

    loops

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
+    let row = '';
     for (let j = 0; j < size; j++) {
-      console.log('X');
+      row += 'X';
     }
+    console.log(row);
   }
 }
