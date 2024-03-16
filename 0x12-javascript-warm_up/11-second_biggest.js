83b2d8efb2b75ae54c93358675e6b1d810fd5ed5
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-16 23:44:55 +0100

    fix: indentation error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-console.log('0');
+  console.log('0');
 } else {
-const compare = process.argv.slice(2).map(Number);
-const nums = compare.sort(function (a, b) {
+  const compare = process.argv.slice(2).map(Number);
+  const nums = compare.sort(function (a, b) {
     return b - a;
 })[1];
-console.log(nums);
+  console.log(nums);
 }
