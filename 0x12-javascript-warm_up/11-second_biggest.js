1f3ec5c56e9ca65d95674c1a13755b1a2a343a06
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-16 23:48:39 +0100

    fix an error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const compare = process.argv.slice(2).map(Number);
-  const nums = compare.sort(function (a, b) {
+  args.sort(function (a, b) {
     return b - a;
-})[1];
-  console.log(nums);
+});
+  console.log(args[1]);
 }
