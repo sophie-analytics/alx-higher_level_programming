2103ba3c648ec8c16cc4129beae76a5f689c945a
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-17 00:58:25 +0100

    fixed an error

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
--- script that lists all records of the table second_table of the database hbtn_0c_0
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
